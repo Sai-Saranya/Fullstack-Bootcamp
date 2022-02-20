b159ce3d5aa3d5fd896dcbf273dd9c7605044d96
Sai-Saranya <52754896+Sai-Saranya@users.noreply.github.com>
2022-02-20 23:55:00 +0530

    modular code

--- utilityApps.js
+++ utilityApps.js
@@ -73,11 +73,8 @@ function hex2rgb(hex)
     return [ r, g, b ]
 }
 
-const choice= readLineSync.question("Enter 1 to perform URL encoding and decoding.\nEnter 2 to perform Base64 Encoding and decoding.\nEnter 3 to perform hashing using various techniques.\nEnter 4 for Epoch converters.\nEnter 5 to perform Binary/Decimal/Octal/Hexa conversions.\nEnter 6 to perform RGG <=> Hexa conversions.\nEnter 7 to perform Unit conversions.\n");
-
-switch(choice){
-    case "1":
-        const option1 = readLineSync.question("Welcome to the URL Encoder Decoder tool\n Enter 1 to encode URL \n Enter 2 to decode URL\n");
+function urlEncoding(){
+    const option1 = readLineSync.question("Welcome to the URL Encoder Decoder tool\n Enter 1 to encode URL \n Enter 2 to decode URL\n");
         switch(option1)
         {
             case "1":
@@ -91,75 +88,79 @@ switch(choice){
             default:
             console.log(`${option} is not a valid choice\n Make sure you enter valid option next time.`);
         }
-        break;
-    case "2":
-        const option2 = readLineSync.question("Welcome to the URL Encoder Decoder tool\n Enter 1 to perform base64Encode URL \n Enter 2 to base64Decode URL\n");
-        switch(option2)
+}
+
+function base64Encoding(){
+    const option2 = readLineSync.question("Welcome to the URL Encoder Decoder tool\n Enter 1 to perform base64Encode URL \n Enter 2 to base64Decode URL\n");
+    switch(option2)
+    {
+        case "1":
+            const URLToEncode=readLineSync.question("You have chosen Encode URL option\nEnter the URL to be encoded\n");
+            console.log(base64encodeURL(URLToEncode));
+            break;
+        case "2":
+            const URLToDecode=readLineSync.question("You have chosen Decode URL option\nEnter the URL to be decoded\n");
+            console.log(base64decodeURL(URLToDecode));
+            break;
+        default:
+            console.log(`${option} is not a valid choice\n Make sure you enter valid option next time.`);
+            break;
+    }
+}
+
+function stringHashing(){
+    const option3 = readLineSync.question("Welcome to the string hashing tool\nEnter 1 to hash using md5 \nEnter 2 to hash using sha-1\nEnter 3 to hash using sha-256\nEnter 4 to hash using sha-512\n");
+    if(!options[option3])
+    {
+        console.log("Please make sure you have chosen valid option\n");
+    }
+    else
+    {
+        const string = readLineSync.question(`Enter string to hash using ${options[option3]}\n`);
+        switch(option3)
         {
             case "1":
-                const URLToEncode=readLineSync.question("You have chosen Encode URL option\nEnter the URL to be encoded\n");
-                console.log(base64encodeURL(URLToEncode));
+                hashString(string,'md5');
                 break;
             case "2":
-                const URLToDecode=readLineSync.question("You have chosen Decode URL option\nEnter the URL to be decoded\n");
-                console.log(base64decodeURL(URLToDecode));
+                hashString(string,'sha1');
+                break;
+            case "3":
+                hashString(string,'sha256');
+                break;
+            case "4":
+                hashString(string,'sha512');
                 break;
             default:
-                console.log(`${option} is not a valid choice\n Make sure you enter valid option next time.`);
-                break;
+                console.log("You have chosen invalid option\n"); 
         }
-        break;
-    case "3":
-        const option3 = readLineSync.question("Welcome to the string hashing tool\nEnter 1 to hash using md5 \nEnter 2 to hash using sha-1\nEnter 3 to hash using sha-256\nEnter 4 to hash using sha-512\n");
-        if(!options[option3])
-        {
-            console.log("Please make sure you have chosen valid option\n");
-        }
-        else
-        {
-            const string = readLineSync.question(`Enter string to hash using ${options[option3]}\n`);
-            switch(option3)
-            {
-                case "1":
-                    hashString(string,'md5');
-                    break;
-                case "2":
-                    hashString(string,'sha1');
-                    break;
-                case "3":
-                    hashString(string,'sha256');
-                    break;
-                case "4":
-                    hashString(string,'sha512');
-                    break;
-                default:
-                    console.log("You have chosen invalid option\n"); 
-            }
-        }
-        break;
-    case "4":
-        const option4=parseInt(readLineSync.question("Enter 1 to convert milliseconds to humanreadable date.\nEnter 2 to convert humanreadable date to milliseconds\n"));
-        switch(option4)
-        {
-            case 1:
-                const ms=parseInt(readLineSync.question("Enter the milliseconds\n"));
-                console.log(toHumanDate(ms));
-                break;
-            case 2:
-                const year = readLineSync.question("Enter the year\n");
-                const month = readLineSync.question("Enter the month \n");
-                const date = readLineSync.question("Enter the date\n");
-                const hours = readLineSync.question("Enter the hours\n");
-                const minutes = readLineSync.question("Enter the minutes\n");
-                const seconds = readLineSync.question("Enter the seconds\n");
-                console.log(toEpoch(year,month,date,hours,minutes,seconds));
-                break;
-            default:
-                console.log("Make sure you have entered correct option");
-        }
-        break;
-    case "5":
-        let num;
+    }
+}
+
+function epochConverter(){
+    const option4=parseInt(readLineSync.question("Enter 1 to convert milliseconds to humanreadable date.\nEnter 2 to convert humanreadable date to milliseconds\n"));
+    switch(option4)
+    {
+        case 1:
+            const ms=parseInt(readLineSync.question("Enter the milliseconds\n"));
+            console.log(toHumanDate(ms));
+            break;
+        case 2:
+            const year = readLineSync.question("Enter the year\n");
+            const month = readLineSync.question("Enter the month \n");
+            const date = readLineSync.question("Enter the date\n");
+            const hours = readLineSync.question("Enter the hours\n");
+            const minutes = readLineSync.question("Enter the minutes\n");
+            const seconds = readLineSync.question("Enter the seconds\n");
+            console.log(toEpoch(year,month,date,hours,minutes,seconds));
+            break;
+        default:
+            console.log("Make sure you have entered correct option");
+    }
+}
+
+function converter(){
+    let num;
         const option5= readLineSync.question("Enter 1 to convert decimal to hexa.\nEnter 2 to convert decimal to octal.\nEnter 3 to convert decimal to binary.\nEnter 4 to convert binary to hexa.\nEnter 5 to convert binary to octal.\nEnter 6 to convert binary to decimal.\nEnter 7 to convert hexa to binary.\nEnter 8 to convert hexa to octal.\nEnter 9 to convert hexa to decimal.\nEnter 10 to convert octal to hexa.\nEnter 11 to convert octal to binary.\nEnter 12 to convert octal to decimal.\n");
 
         switch(option5)
@@ -222,9 +223,10 @@ switch(choice){
                 console.log("Make sure you entered correct option.\n");
                 break;
         }
-        break;
-    case "6":
-        const opt6 = readLineSync.question("Welcome to tool to convert RGB to Hexa and viceversa\nEnter 1 to convert RGB to Hexa.\nEnter 2 to convert Hexa to RGB.\n");
+}
+
+function rgbHexaConverter(){
+    const opt6 = readLineSync.question("Welcome to tool to convert RGB to Hexa and viceversa\nEnter 1 to convert RGB to Hexa.\nEnter 2 to convert Hexa to RGB.\n");
         switch(opt6)
         {
             case "1":
@@ -240,9 +242,10 @@ switch(choice){
             default:
                 console.log("Make sure you entered correct option.\n");
         }
-        break;
-    case "7":
-        let num1;
+}
+
+function kmConverter(){
+    let num1;
         const opt7 = readLineSync.question("Enter 1 to convert kms to mts.\nEnter 2 to convert kms to cms.\nEnter 3 to convert kms to mms.\n");
         switch(opt7)
         {
@@ -262,6 +265,30 @@ switch(choice){
                 console.log("Make sure you have entered valid option.\n");
                 break;
         }
+}
+const choice= readLineSync.question("Enter 1 to perform URL encoding and decoding.\nEnter 2 to perform Base64 Encoding and decoding.\nEnter 3 to perform hashing using various techniques.\nEnter 4 for Epoch converters.\nEnter 5 to perform Binary/Decimal/Octal/Hexa conversions.\nEnter 6 to perform RGB <=> Hexa conversions.\nEnter 7 to perform Unit conversions.\n");
+
+switch(choice){
+    case "1":
+        urlEncoding();
+        break;
+    case "2":
+        base64Encoding();
+        break;
+    case "3":
+        stringHashing();
+        break;
+    case "4":
+        epochConverter()
+        break;
+    case "5":
+        converter();
+        break;
+    case "6":
+        rgbHexaConverter();
+        break;
+    case "7":
+        kmConverter();
         break;
     default:
         console.log("Get Lost!\n");
